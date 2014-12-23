35a8b0035cd3adeb78dac5cce09091303843abec
thegreenhornet <bigboimemo@gmail.com>
2014-12-23 12:25:43 +0300

    Added contactus.php

---
+++ contactus.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.2.0-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.2.0-dist/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div style="height:4px;"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<div id="header"><img height="200" src="logo.jpg" /></div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<div id="menu-wrapper">
+					<?php require_once "modules/menu.php"; ?>
+				</div>
+			</div>
+		</div>
+		<div class="row" id="content-body">
+			<div class="col-sm-8">
+				<div id="content-left"> <!--Content left col-->
+					<div class="row contacts-container">
+						<div class="col-sm-12">
+						<div class="">
+							<h3>Contact Us</h3>
+							<div class="contact-details">
+								<label>Phone: +244 3434322431</label> <br>
+								<label>Email: Oilpresstz@hotmail.com</label><br>
+								<label>Website: www.oilgastanzania.com</label><br>
+							</div>
+
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div id="content-right"> <!-- Content right col-->
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<div id="footer"></div>
+			</div>
+		</div>
+	</div>
+</body>
+<script type="text/javascript" src="bootstrap-3.2.0-dist/js/jquery.min.js"></script>
+<script type="text/javascript" src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+</html>
